bbc05150a8f85dd2ca9fcc9a675d0aac1fae32c7
KirillDemyanenko <demyanenkokg@gmail.com>
2024-09-18 15:52:28 +0300

    feat: implement solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const pairs = {};
+  const open = [];
+  let isValid = true;
+  bracketsConfig.forEach((item) => (pairs[item.at(0)] = item.at(1)));
+  str.split("").forEach((sym) => {
+    if (Object.keys(pairs).includes(sym)) {
+      open.push(sym);
+    } else if (Object.values(pairs).includes(sym)) {
+      if (open.length === 0 || pairs[open.pop()] !== sym) {
+        isValid = false;
+      }
+    }
+  });
+  return isValid && open.length === 0;
+};
